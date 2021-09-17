86290907ce69e9edf8d6bc4f1bb032c79133bb43
Libor Knappek <libor.knappek@gmail.com>
2021-09-17 12:12:39 +0200

    V3

--- my_app.py
+++ my_app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 #Instantiate Flask
 app = Flask(__name__)
@@ -11,6 +13,51 @@ app.config.update(
 )
 
 db = SQLAlchemy(app)
+
+class Publication(db.Model):
+    __tablename__="publication"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __init__(self,name):
+        self.name = name
+    
+    def __repr__(self):
+        return f"The publisher name is:{self.name}"
+
+
+class Book(db.Model):
+    '''
+    avg_rating = rating, float
+    format = publishing format (ebook, hardcover, paperback), str
+    image = link to book image, str 
+    num_pages = number of pages, int
+    pub_id = id of the publisher, int
+    '''
+    __tablename__="book"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+    #relationship
+    pub_id = db.Column(db.Integer, db.ForeignKey("publication.id"))
+
+    def __init__(self, title,author, avg_rating, format, image, num_pages, pub_id):
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+    
+    def __repr__(self):
+        return f"{self.title} by {self.author}"
+
 
 @app.route("/index")
 #Route
@@ -70,20 +117,13 @@ def filters():
 @app.route("/macros")
 def macros():
     movies_dict = {"This is my goal":3.09, "More than this":2.5, "My stalled engine":2.2,"You and him":0.8}
+    # pub = Publication(105,"Python Publishing Inc.")
+    # db.session.add(pub)
+    # db.session.commit()   
     return render_template("using_macros.html", movies=movies_dict)
 
 
-class Publication(db.Model):
-    __tablename__="publication"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self,id,name):
-        self.id = id
-        self.name = name
-    
-    def __repr__(self):
-        return f"The id is: {self.id}, the name is:{self.name}"
 
 #boilerplate
 if __name__ == "__main__":
